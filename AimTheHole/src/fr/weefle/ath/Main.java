bd6172f6ebbafd93dcc1a12ed00577e85a52ecfc
Weefle <mr.weefle@gmail.com>
2017-06-04 20:51:08 +0200

    Ts3 test

--- AimTheHole/src/fr/weefle/ath/Main.java
+++ AimTheHole/src/fr/weefle/ath/Main.java
@@ -5,6 +5,10 @@ import java.util.UUID;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.github.theholywaffle.teamspeak3.TS3Api;
+import com.github.theholywaffle.teamspeak3.TS3Config;
+import com.github.theholywaffle.teamspeak3.TS3Query;
+
 public class Main extends JavaPlugin {
 	
 	public static Main instance;
@@ -18,6 +22,17 @@ public class Main extends JavaPlugin {
 		ATHEvents.registerEvents();
 		ATHCommands.registerCommands();
 		CreateConfig.createConfig();
+		final TS3Config config = new TS3Config();
+		config.setHost("77.77.77.77");
+
+		final TS3Query query = new TS3Query(config);
+		query.connect();
+
+		final TS3Api api = query.getApi();
+		api.login("serveradmin", "serveradminpassword");
+		api.selectVirtualServerById(1);
+		api.setNickname("PutPutBot");
+		api.sendChannelMessage("PutPutBot is online!");
 		super.onEnable();
 	}
 
